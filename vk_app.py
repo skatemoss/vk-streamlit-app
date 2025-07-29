3f02558808069b8eaf640bd10f2c25693c5a5c34
Mikhail K. <45450732+skatemoss@users.noreply.github.com>
2025-07-29 13:34:01 +0300

    Update vk_app.py

--- vk_app.py
+++ vk_app.py
@@ -436,7 +436,7 @@ if uploaded:
 
             for user in users_resp.get("response", []):
                 user_result = {
-                    user_result[id_col] = user.get("id"),
+                    user_result[id_col] = user.get("id")
                     "first_name": user.get("first_name"),
                     "last_name": user.get("last_name"),
                     "activities": user.get("activities"),
